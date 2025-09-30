6d443f3722c6ee1a00ed38d9910c4eb48224bfab
valentinamartireta <vmartireta@itba.edu.ar>
2025-09-30 16:04:35 -0300

    comments refactoring

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import sys
 from exercises import ex1, ex2
 
 EXPERIMENTS = {
-    "ej1": ex1.run,
-    "ej2": ex2.run,
+    "exercise1": ex1.run,
+    "exercise2": ex2.run,
 }
 
 def run_experiment(name: str):
@@ -19,10 +19,10 @@ def main():
 
     experiment_name = sys.argv[1]
 
-    if experiment_name == "ej1":
-        EXPERIMENTS["ej1"]()
+    if experiment_name == "exercise1":
+        EXPERIMENTS["exercise1"]()
 
-    elif experiment_name == "ej2":
+    elif experiment_name == "exercise2":
         if len(sys.argv) < 4:
             print("Usage: python main.py ej2 <path_to_csv_file> <model_type>")
             print("       <model_type> options: 'lineal' (Regression) or 'no_lineal' (Classification)")
@@ -31,7 +31,7 @@ def main():
         csv_file_path = sys.argv[2]
         model_type = sys.argv[3]
 
-        EXPERIMENTS["ej2"](csv_file_path, model_type)
+        EXPERIMENTS["exercise2"](csv_file_path, model_type)
 
     else:
         print(f"Experiment '{experiment_name}' not recognized.")
